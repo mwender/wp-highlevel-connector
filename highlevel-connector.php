676d2fe75ec9f2727a983e0026313461dbcdbfd7
Michael Wender <michael@michaelwender.com>
2021-01-11 14:05:50 -0500

    First commit

---
+++ highlevel-connector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     HighLevel Connector
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Provides various connections between WordPress and HighLevel CRM
+ * Author:          Michael Wender
+ * Author URI:      https://mwender.com
+ * Text Domain:     highlevel-connector
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Highlevel_Connector
+ */
+
+// Your code starts here.
